7bba8815d68ec55d76f84f0ac41d6e86e36c844d
Tyrese1 <obatarhe.otughwor@gmail.com>
2018-09-03 21:15:21 +0100

    updates server file bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import Question from './src/controllers/Question';
 import Answer from './src/controllers/Answer';
-import Auth from './src/controllers/auth';
+
 
 const app = express()
 
@@ -10,17 +10,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'New Andela endpoint project'});
 })
-//Signup and login operation endpoint
-// app.post('/api/auth/signup', Question.create);
-// app.post('/api/auth/login', Question.create);
-
-// const userController = require('../src/controllers/user');
-
 
 //Question operarion endpoint
-app.post('/api/v1/auth', Auth.create);
-app.get('/api/v1/auth', Auth.getAll);
-app.get('/api/v1/auth/:id', Auth.getOne);
 app.post('/api/v1/questions/:id/answers', Question.create);
 app.post('/api/v1/questions', Question.create);
 app.get('/api/v1/questions', Question.getAll);
